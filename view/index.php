11673e675a7ee5049a1188bc39ddee0a3e6a3642
Austin Juntra <AustinJuntra@gmail.com>
2014-11-04 08:54:10 -0800

    new file

--- view/index.php
+++ view/index.php
@@ -6,8 +6,10 @@
 	</head>
 	<body>
 	<?php
-	   require_once(__DIR__ . "/view/header.php");
+	   require_once(__DIR__ . "/view/header.php");//checks for file
 	   require_once(__DIR__ . "/controller/create_db.php");//look for file
+	   require_once(__DIR__ . "/view/footer.php");//checks for file
+	   require_once(__DIR__ . "/view/navigation.php"); //new for navigation bar 
 	?>
 	</body>
 </html>
