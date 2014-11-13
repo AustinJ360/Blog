f4cec80f7cd4816495c7e5c2ea65010759b00218
Austin Juntra <AustinJuntra@gmail.com>
2014-11-13 08:54:05 -0800

    submitting post to phpmyadmin

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,8 +1,17 @@
 <?php //creates a play so title and post can be sent to. 
+require_once(__DIR__ . "/../model/database.php"); //so we have access
+$connection = new mysqli($host, $username, $password, $database);
 $title = filter_input(INPUT_POST, "title",FILTER_SANITIZE_STRING);//filter the input to make sure there is no mellicious things.
 $post = filter_input(INPUT_POST, "post",FILTER_SANITIZE_STRING);
 
-echo "<p>Title: $title</p>";//to echo out title.
-echo "<p>Post: $post</p>";
+$query = $connection->query("INSERT INTO posts SET title = '$title', post = '$post'");
+if($query){
+echo "<p>Successfully inserted post : $title</p>";
+}
+else{
+echo "<p>$connection->error</p>";
+}
+
+$connection->close();
 
 ?>
---
+++ view/header.php
@@ -0,0 +1,3 @@
+<link type="text/css" href="header.php">
+
+<meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> 
