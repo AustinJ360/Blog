b3cd60b86a65618a5978a1f376f7bc5bbed30a75
Austin Juntra <AustinJuntra@gmail.com>
2014-11-17 10:30:31 -0800

    media query

---
+++ model/Database.php
@@ -0,0 +1,19 @@
+<?php
+class Database{//Classes are a convenient way to group data and functionality.You could make a class to contain a persons information.
+//to create your own object to make variables and use function.  a Class is a blueprint for an object.
+//A class is a collection of variables and functions working with these variables. Variables are defined by var and functions by function. 
+
+ private $connection;//variable only able to access within this file
+ private $host;
+ private $username;
+ private $password;
+ private $database;
+
+ public function __construct($host, $username, $password, $database){
+ 	$this->host = $host;//access to all global variables
+ 	$this->username = $username;
+ 	$this->password = $password;
+ 	$this->database = $database;
+ }
+}
+?> 
