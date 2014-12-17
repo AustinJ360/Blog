d8e385a3a75aebdcf5a605a9dd6a32fdf59a2203
Austin Juntra <AustinJuntra@gmail.com>
2014-12-17 08:01:57 -0800

    almost finished blog

--- view/header.php
+++ view/header.php
@@ -5,7 +5,8 @@
 		<meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> 
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<title>Life of the World</title>
+	
+
 	
 	</head>
 	<body>
